6c9bd59ef331c04b3e08d860e0f4b18413715c5e
viet-intel <vietnhprivat@gmail.com>
2023-07-24 15:49:59 +0200

    Webscraping works but is slow and runs in single

--- src/node.js
+++ src/node.js
@@ -1,29 +1,94 @@
+const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
+const mysql = require('mysql2/promise');
 
-// HTML text
-const html_text = `
-<li><strong>Adresse: </strong>Anker Heegaards Gade 1B, 2., 1572 København V</li>
-<li><strong>BFE: </strong>172356</li>
-<li><strong>Kommune: </strong>København (0101)</li>
-<li><strong>Matrikel-ejerlav: </strong>291 - Vestervold Kvarter, København</li>
-<li><strong>Grund-areal: </strong>1.215 m²</li>
-<li><strong>Bebygget areal: </strong>901 m²</li>
-<li><strong>Byggesager: </strong>Ja</li>
-<li><strong>Administrator: </strong></li>
-<li><strong>Ejer: </strong>Kampmannsgaard P/S</li>
-<li><strong>Vurdering: </strong>2.250.000 (2022)</li>
-<li><strong>Salgspris: </strong></li>
-`;
 
-const $ = cheerio.load(html_text);
-$('li').each(function(i, elem) {
-  const text = $(this).text();
-  const field = text.split(':')[0];
-  
-  // List of fields you are interested in
-  const interestedFields = ["Adresse", "BFE", "Matrikel-ejerlav", "Grund-areal", "Bebygget areal", "Byggesager", "Administrator", "Ejer", "Vurdering", "Salgspris"];
+(async () => {
+  console.time("Execution time");  // Start the timer
+  // Connecting to MySQL
+  const connection = await mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '1234',
+    database: 'bolig',
+    namedPlaceholders: true
+  });
 
-  if (interestedFields.includes(field)) {
-    console.log(text);
+  // Selecting rows from the ebr table
+  const [rows] = await connection.execute('SELECT id_lokalid, bestemtFastEjendomBFENr FROM ebr WHERE ois = false LIMIT 100');
+
+  const browser = await puppeteer.launch();
+
+  for (let row of rows) {
+    const page = await browser.newPage();
+
+    // Construct the URL using bestemtFastEjendomBFENr
+    const url = `https://nyt.ois.dk/search/${row.bestemtFastEjendomBFENr}`;
+
+    await page.goto(url);
+    await page.waitForTimeout(2000);
+
+    const data = await page.evaluate(() => {
+      const ulElement = document.querySelector('ul.grid');
+      return ulElement ? ulElement.innerHTML : '';
+    });
+
+    const $ = cheerio.load(data);
+
+    // Initialize an object to hold the scraped data
+    let scrapedData = {
+      id: row.id_lokalid,
+      Adresse: null,
+      BFE: null,
+      Matrikel_ejerlav: null,
+      Grund_areal: null,
+      Bebygget_areal: null,
+      Byggesager: null,
+      Administrator: null,
+      Ejer: null,
+      Vurdering: null,
+      Salgspris: null
+    };
+
+    $('li').each(function(i, elem) {
+      const text = $(this).text();
+      const field = text.split(':')[0];
+      const value = text.split(':')[1] ? text.split(':')[1].trim() : null;
+
+      const interestedFields = ["Adresse", "BFE", "Matrikel-ejerlav", "Grund-areal", "Bebygget areal", "Byggesager", "Administrator", "Ejer", "Vurdering", "Salgspris"];
+
+      const fieldConversion = {
+        "Adresse": "Adresse",
+        "BFE": "BFE",
+        "Matrikel-ejerlav": "Matrikel_ejerlav",
+        "Grund-areal": "Grund_areal",
+        "Bebygget areal": "Bebygget_areal",
+        "Byggesager": "Byggesager",
+        "Administrator": "Administrator",
+        "Ejer": "Ejer",
+        "Vurdering": "Vurdering",
+        "Salgspris": "Salgspris"
+      };
+
+      if (interestedFields.includes(field)) {
+        // Add the field and its corresponding value to the scrapedData object
+        scrapedData[fieldConversion[field]] = value;
+      }
+    });
+
+    // Insert the scraped data into the ois table
+    await connection.execute('INSERT INTO ois (id, Adresse, BFE, Matrikel_ejerlav, Grund_areal, Bebygget_areal, Byggesager, Administrator, Ejer, Vurdering, Salgspris) VALUES (:id, :Adresse, :BFE, :Matrikel_ejerlav, :Grund_areal, :Bebygget_areal, :Byggesager, :Administrator, :Ejer, :Vurdering, :Salgspris)', scrapedData);
+
+    // Update the ois column in the ebr table
+    await connection.execute('UPDATE ebr SET ois = true WHERE id_lokalid = ?', [row.id_lokalid]);
+
+    await page.close();
   }
-});
+
+  await browser.close();
+  await connection.end();
+
+  // Print done when the script is finished
+  console.timeEnd("Execution time");  // End the timer and print the elapsed time
+  console.log("done");
+})();
