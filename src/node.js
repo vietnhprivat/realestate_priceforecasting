7912a6cd56fa29cb46b656fff80754baa1a845d4
viet-intel <vietnhprivat@gmail.com>
2023-07-24 16:11:57 +0200

    Webscraping parallel working

--- src/node.js
+++ src/node.js
@@ -1,11 +1,13 @@
 const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
 const mysql = require('mysql2/promise');
+const { PromisePool } = require('@supercharge/promise-pool');
 
+const CONCURRENCY = 50;
 
 (async () => {
-  console.time("Execution time");  // Start the timer
-  // Connecting to MySQL
+  console.time("Execution time");
+
   const connection = await mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -14,81 +16,75 @@ const mysql = require('mysql2/promise');
     namedPlaceholders: true
   });
 
-  // Selecting rows from the ebr table
   const [rows] = await connection.execute('SELECT id_lokalid, bestemtFastEjendomBFENr FROM ebr WHERE ois = false LIMIT 100');
 
   const browser = await puppeteer.launch();
 
-  for (let row of rows) {
-    const page = await browser.newPage();
+  const { results, errors } = await PromisePool
+    .for(rows)
+    .withConcurrency(CONCURRENCY)
+    .process(async row => {
+      const page = await browser.newPage();
+      const url = `https://nyt.ois.dk/search/${row.bestemtFastEjendomBFENr}`;
 
-    // Construct the URL using bestemtFastEjendomBFENr
-    const url = `https://nyt.ois.dk/search/${row.bestemtFastEjendomBFENr}`;
+      await page.goto(url);
+      await page.waitForTimeout(15000);
 
-    await page.goto(url);
-    await page.waitForTimeout(2000);
+      const data = await page.evaluate(() => {
+        const ulElement = document.querySelector('ul.grid');
+        return ulElement ? ulElement.innerHTML : '';
+      });
 
-    const data = await page.evaluate(() => {
-      const ulElement = document.querySelector('ul.grid');
-      return ulElement ? ulElement.innerHTML : '';
-    });
+      const $ = cheerio.load(data);
 
-    const $ = cheerio.load(data);
-
-    // Initialize an object to hold the scraped data
-    let scrapedData = {
-      id: row.id_lokalid,
-      Adresse: null,
-      BFE: null,
-      Matrikel_ejerlav: null,
-      Grund_areal: null,
-      Bebygget_areal: null,
-      Byggesager: null,
-      Administrator: null,
-      Ejer: null,
-      Vurdering: null,
-      Salgspris: null
-    };
-
-    $('li').each(function(i, elem) {
-      const text = $(this).text();
-      const field = text.split(':')[0];
-      const value = text.split(':')[1] ? text.split(':')[1].trim() : null;
-
-      const interestedFields = ["Adresse", "BFE", "Matrikel-ejerlav", "Grund-areal", "Bebygget areal", "Byggesager", "Administrator", "Ejer", "Vurdering", "Salgspris"];
-
-      const fieldConversion = {
-        "Adresse": "Adresse",
-        "BFE": "BFE",
-        "Matrikel-ejerlav": "Matrikel_ejerlav",
-        "Grund-areal": "Grund_areal",
-        "Bebygget areal": "Bebygget_areal",
-        "Byggesager": "Byggesager",
-        "Administrator": "Administrator",
-        "Ejer": "Ejer",
-        "Vurdering": "Vurdering",
-        "Salgspris": "Salgspris"
+      let scrapedData = {
+        id: row.id_lokalid,
+        Adresse: null,
+        BFE: null,
+        Matrikel_ejerlav: null,
+        Grund_areal: null,
+        Bebygget_areal: null,
+        Byggesager: null,
+        Administrator: null,
+        Ejer: null,
+        Vurdering: null,
+        Salgspris: null
       };
 
-      if (interestedFields.includes(field)) {
-        // Add the field and its corresponding value to the scrapedData object
-        scrapedData[fieldConversion[field]] = value;
-      }
+      $('li').each(function(i, elem) {
+        const text = $(this).text();
+        const field = text.split(':')[0];
+        const value = text.split(':')[1] ? text.split(':')[1].trim() : null;
+
+        const interestedFields = ["Adresse", "BFE", "Matrikel-ejerlav", "Grund-areal", "Bebygget areal", "Byggesager", "Administrator", "Ejer", "Vurdering", "Salgspris"];
+
+        const fieldConversion = {
+          "Adresse": "Adresse",
+          "BFE": "BFE",
+          "Matrikel-ejerlav": "Matrikel_ejerlav",
+          "Grund-areal": "Grund_areal",
+          "Bebygget areal": "Bebygget_areal",
+          "Byggesager": "Byggesager",
+          "Administrator": "Administrator",
+          "Ejer": "Ejer",
+          "Vurdering": "Vurdering",
+          "Salgspris": "Salgspris"
+        };
+
+        if (interestedFields.includes(field)) {
+          scrapedData[fieldConversion[field]] = value;
+        }
+      });
+
+      await connection.execute('INSERT INTO ois (id, Adresse, BFE, Matrikel_ejerlav, Grund_areal, Bebygget_areal, Byggesager, Administrator, Ejer, Vurdering, Salgspris) VALUES (:id, :Adresse, :BFE, :Matrikel_ejerlav, :Grund_areal, :Bebygget_areal, :Byggesager, :Administrator, :Ejer, :Vurdering, :Salgspris)', scrapedData);
+      await connection.execute('UPDATE ebr SET ois = true WHERE id_lokalid = ?', [row.id_lokalid]);
+
+      await page.close();
     });
-
-    // Insert the scraped data into the ois table
-    await connection.execute('INSERT INTO ois (id, Adresse, BFE, Matrikel_ejerlav, Grund_areal, Bebygget_areal, Byggesager, Administrator, Ejer, Vurdering, Salgspris) VALUES (:id, :Adresse, :BFE, :Matrikel_ejerlav, :Grund_areal, :Bebygget_areal, :Byggesager, :Administrator, :Ejer, :Vurdering, :Salgspris)', scrapedData);
-
-    // Update the ois column in the ebr table
-    await connection.execute('UPDATE ebr SET ois = true WHERE id_lokalid = ?', [row.id_lokalid]);
-
-    await page.close();
-  }
 
   await browser.close();
   await connection.end();
 
-  // Print done when the script is finished
-  console.timeEnd("Execution time");  // End the timer and print the elapsed time
+  console.timeEnd("Execution time");
   console.log("done");
 })();
